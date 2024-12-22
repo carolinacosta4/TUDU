11c270652c6cfd3c313de2a8f25d299040a59f50
Carolina Costa <carolinamccosta@gmail.com>
2024-12-22 01:46:34 +0000

    initial commit app

---
+++ app/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function HomeScreen() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
