e1cdebb2eba3fedc4cca0b2c084355a3747b70cd
Carolina Costa <carolinamccosta@gmail.com>
2024-12-29 03:59:03 +0000

    recover password page done

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="register" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="recoverPassword" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
