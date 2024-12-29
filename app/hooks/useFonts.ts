b3a68ca667e06325e57bbe0a1eb5303ac4dd0ce2
Carolina Costa <carolinamccosta@gmail.com>
2024-12-29 04:21:08 +0000

    starting homepage

--- app/hooks/useFonts.ts
+++ app/hooks/useFonts.ts
@@ -9,6 +9,7 @@ const useFonts = () => {
       try {
         await Font.loadAsync({
           "Rebond-Grotesque-Regular": require("@/assets/fonts/ESRebondGrotesqueTRIAL-Regular-BF66189040b697b.otf"),
+          "Rebond-Grotesque-Medium": require("@/assets/fonts/ESRebondGrotesqueTRIAL-Medium-BF6618904088566.otf"),
           "Rebond-Grotesque-Bold": require("@/assets/fonts/ESRebondGrotesqueTRIAL-Bold-BF66189040400df.otf"),
         });
         setFontsLoaded(true);
