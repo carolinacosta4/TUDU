285f43dae9ba2f8e44002c649bd11cdb90909995
Carolina Costa <carolinamccosta@gmail.com>
2025-01-10 13:47:24 +0000

    reanimated

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,18 +1,26 @@
 import { Stack } from "expo-router";
 import "react-native-reanimated";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { configureReanimatedLogger } from "react-native-reanimated";
+
+configureReanimatedLogger({
+  strict: false,
+});
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="register" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="recoverPassword" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="resetPassword/[id]"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <GestureHandlerRootView>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="recoverPassword" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="resetPassword/[id]"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
